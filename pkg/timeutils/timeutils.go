8750a5f808d06b4494232fb70219510fd2a7e493
Jerry <jianglei02@inspur.com>
2019-08-13 09:19:51 +0800

    1. add Zone Info around the world. 2. timeutils add parse functions.

--- pkg/timeutils/timeutils.go
+++ pkg/timeutils/timeutils.go
@@ -1,3 +1,12 @@
+/**
+* @Author       :jerry
+* @Version      :1.0.0
+* @Date         :下午7:12 19-8-12
+* @Description  : timeutils to help you
+				(1) get time in specified type,
+				(2) and format time to specified pattern,
+				(3) parse time with specified pattern.
+*/
 package timeutils
 
 import (
@@ -19,7 +28,7 @@ return formatted string from Now time-object within Golang builtin type `time.Ti
 Predefined layouts ANSIC, UnixDate, RFC3339 and others describe standard
 and convenient representations of the reference time. For more information
 about the formats and the definition of the reference time, see the
-documentation for ANSIC and the other constants defined by package @time
+documentation for ANSIC and the other constants defined by package  time
 */
 func NowTimeStringWithinPattern(pattern string) string {
 	now := time.Now()
@@ -28,6 +37,10 @@ func NowTimeStringWithinPattern(pattern string) string {
 
 	return resultStr
 }
+
+/**
+return formatted string from specified parameter @param t using pattern.
+*/
 func FormetTime(t time.Time, pattern string) string {
 
 	return t.Format(pattern)
@@ -144,4 +157,60 @@ e.g, Now is 2019-01-10 10:29:56, then return 56
 */
 func NowSecond() int {
 	return time.Now().Second()
+}
+
+/**
+ ParseTime parses a formatted string and returns the time value it represents.
+ The layout defines the format by showing how the reference time,
+ defined to be
+	Mon Jan 2 15:04:05 -0700 MST 2006
+ would be interpreted if it were the value; it serves as an example of
+ the input format. The same interpretation will then be made to the
+ input string.
+
+ Predefined layouts ANSIC, UnixDate, RFC3339 and others describe standard
+ and convenient representations of the reference time. For more information
+ about the formats and the definition of the reference time, see the
+ documentation for ANSIC and the other constants defined by this package.
+ Also, the executable example for Time.Format demonstrates the working
+ of the layout string in detail and is a good reference.
+
+ Elements omitted from the value are assumed to be zero or, when
+ zero is impossible, one, so parsing "3:04pm" returns the time
+ corresponding to Jan 1, year 0, 15:04:00 UTC (note that because the year is
+ 0, this time is before the zero Time).
+ Years must be in the range 0000..9999. The day of the week is checked
+ for syntax but it is otherwise ignored.
+
+ In the absence of a time zone indicator, Parse returns a time in UTC.
+
+ When parsing a time with a zone offset like -0700, if the offset corresponds
+ to a time zone used by the current location (Local), then Parse uses that
+ location and zone in the returned time. Otherwise it records the time as
+ being in a fabricated location with time fixed at the given zone offset.
+
+ When parsing a time with a zone abbreviation like MST, if the zone abbreviation
+ has a defined offset in the current location, then that offset is used.
+ The zone abbreviation "UTC" is recognized as UTC regardless of location.
+ If the zone abbreviation is unknown, Parse records the time as being
+ in a fabricated location with the given zone abbreviation and a zero offset.
+ This choice means that such a time can be parsed and reformatted with the
+ same layout losslessly, but the exact instant used in the representation will
+ differ by the actual zone offset. To avoid such problems, prefer time layouts
+ that use a numeric zone offset, or use ParseInLocation.
+*/
+func ParseTime(pattern string, value string) (time.Time, error) {
+	t, err := time.Parse(pattern, value)
+	return t, err
+}
+
+/**
+ParseTimeInLocation is like Parse but differs in two important ways.
+First, in the absence of time zone information, Parse interprets a time as UTC;
+ParseInLocation interprets the time as in the given location.
+Second, when given a zone offset or abbreviation, Parse tries to match it
+against the Local location; ParseInLocation uses the given location.
+*/
+func ParseTimeInLocation(pattern, value string, loc *time.Location) (time.Time, error) {
+	return time.ParseInLocation(pattern, value, loc)
 }
--- pkg/timeutils/timeutils_test.go
+++ pkg/timeutils/timeutils_test.go
@@ -20,3 +20,35 @@ func TestNowMonth(t *testing.T) {
 	fmt.Printf("month: %d", month)
 
 }
+
+func TestParseTime(t *testing.T) {
+	const pATTERN_FULL string = "2006-01-02 15:04:05"
+	timeValue := "2019-08-12 19:57:23"
+	ts, _ := time.Parse(pATTERN_FULL, timeValue)
+	result, _ := ParseTime(pATTERN_FULL, "2019-08-12 19:57:23")
+	fmt.Printf("result: %s", result)
+	if ts != result {
+		t.Errorf("ParseTime gives the wrong result: %s, expected: %s", result, ts)
+	}
+}
+
+func TestParseTimeInLocation(t *testing.T) {
+	const pATTERN_FULL string = "2006-01-02 15:04:05"
+	timeValue := "2019-08-12 19:57:23"
+	ts, _ := time.ParseInLocation(pATTERN_FULL, timeValue, time.Local)
+	result, _ := ParseTimeInLocation(pATTERN_FULL, "2019-08-12 19:57:23", time.Local)
+	fmt.Printf("result: %s", result)
+	if ts != result {
+		t.Errorf("ParseTime gives the wrong result: %s, expected: %s", result, ts)
+	}
+}
+func TestLoadLocation(t *testing.T) {
+	location, _ := time.LoadLocation("Asia/Shanghai")
+	value := "2019-08-12 19:57:23"
+	result, _ := ParseTimeInLocation(pATTERN, value, location)
+	fmt.Printf("result: %s", result)
+	srcTime := time.Date(2019, 8, 12, 19, 57, 23, 0, location)
+	if srcTime != result {
+		t.Errorf("ParseTime gives the wrong result: %s, expected: %s", result, srcTime)
+	}
+}
